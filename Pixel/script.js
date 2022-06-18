dc118738fc0a5d98b71d46db9abed7227fca8512
vadsimus <vadsimus@gmail.com>
2022-06-18 11:18:52 +0300

    add mouseover

--- Pixel/script.js
+++ Pixel/script.js
@@ -1,13 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => { 
 
+    is_mouse_down = false;
+    function mouseEnter(e){
+        console.log(is_mouse_down)
+        if (is_mouse_down) {
+            e.target.classList.toggle('red')
+        }
+    }
+
+
+
+
+    
     document.getElementById("create_field_btn").addEventListener('click', ()=>{
         document.getElementById('create_field_btn').setAttribute('disabled', true)
         let x_input = document.getElementById('input_1')
         let y_input = document.getElementById('input_2')
+        let size_input = document.getElementById('size')
         x_input.setAttribute('disabled', true)
         y_input.setAttribute('disabled', true)
+        size_input.setAttribute('disabled', true)
         let x = x_input.value
         let y = y_input.value
+        let size = size_input.value
 
         let game_field = document.getElementById('game')
         
@@ -17,9 +32,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
             for (let i = 0; i < Number(y); i++) {
                 pixel = document.createElement('div')
-                pixel.addEventListener('click', (e) => {
+                pixel.style.width = size + "px"
+                pixel.style.height = size + "px"
+                pixel.addEventListener('mousedown', (e)=>{
+                    is_mouse_down=true
                     e.target.classList.toggle('red')
                 })
+                pixel.addEventListener('mouseup', (e)=>{
+                    is_mouse_down=false
+                })
+
+
+                pixel.addEventListener('mouseenter', mouseEnter)
                 line.appendChild(pixel)
              }
 
@@ -27,8 +51,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }   
     })
 
-    var reset = document.getElementById("clear_btn");
-    reset.addEventListener("click", function () { //Все сначала
+
+
+    document.getElementById("clear_btn").addEventListener("click", function () {
         location.reload();
     });
 
